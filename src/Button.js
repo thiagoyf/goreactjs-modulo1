729333b9b06c1be7f5c9994bf009a71bd6c90feb
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-05-14 13:36:29 -0400

    Stateful and stateless components

---
+++ src/Button.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+// Form stateful component
+// export default class Button extends Component {
+//   static defaultProps = {
+//     children: "Salvar"
+//   };
+
+//   static propTypes = {
+//     onClick: PropTypes.func.isRequired,
+//     children: PropTypes.string
+//   };
+
+//   render() {
+//     return <button onClick={this.props.onClick}>{this.props.children}</button>;
+//   }
+// }
+
+// Equivalent to the above code
+const Button = props => (
+  <button onClick={props.onClick}>{props.children}</button>
+);
+
+Button.defaultProps = {
+  children: "Salvar"
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.string
+};
+
+export default Button;
