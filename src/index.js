38fa58f064e1e1797424b0ca2b3e8a216a6e540d
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-05-14 13:20:02 -0400

    setState async

--- src/index.js
+++ src/index.js
@@ -13,29 +13,25 @@ class Button extends Component {
   };
 
   render() {
-    return (
-      <a href="" onClick={this.props.onClick}>
-        {this.props.children}
-      </a>
-    );
+    return <button onClick={this.props.onClick}>{this.props.children}</button>;
   }
 }
 
 class App extends Component {
-  handleClick() {
-    alert("Botão clicado");
-  }
+  state = {
+    counter: 0
+  };
+
+  handleClick = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
 
   render() {
     return (
       <Fragment>
         <h1>Hello Rocketseat</h1>
-        <Button
-          onClick={() => {
-            alert("Button 1");
-          }}
-        />
-        <Button onClick={this.handleClick}>Enviar</Button>
+        <h2>{this.state.counter}</h2>
+        <Button onClick={this.handleClick}>Somar</Button>
       </Fragment>
     );
   }
