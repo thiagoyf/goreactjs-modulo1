0c442a20a7baef11fa43a49d95011aaa0154a5ae
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-05-14 14:10:37 -0400

    React lifecycle

--- src/index.js
+++ src/index.js
@@ -8,6 +8,37 @@ class App extends Component {
     counter: 0
   };
 
+  // metodo que é executado na criação do componente
+  componentDidMount() {
+    // perfeito para:
+    // chamada a api
+    // para carregar informações iniciais do componente (listagem)
+    // para cadastrar event listeners (click, change, etc...)
+  }
+
+  // toda vez o componente sofrer uma alteração em uma propriedade ou num estado
+  // esse metodo é chamado, ela permite chamar o render ou não
+  // executado antes de 'render'
+  // usado para controlar a performance do componente
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.counter <= 10;
+  }
+
+  // executado após 'render'
+  // executado depois das atualizações terem ocorrido
+  // recebe as informações antes delas atualizarem
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevState.counter >= 9) {
+      this.setState({ counter: 0 });
+    }
+  };
+
+  // metodo que o componente chama quando vai deixar de existir na nossa aplicação
+  componentWillUnmount() {
+    // usado para:
+    // limpar todo os events listener que tenhamos feito dentro de componentDidMount
+  }
+
   handleClick = () => {
     this.setState({ counter: this.state.counter + 1 });
   };
